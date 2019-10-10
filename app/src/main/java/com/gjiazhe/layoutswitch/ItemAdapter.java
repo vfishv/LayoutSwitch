5d30f81fb40457110afac087d39617bf172ecdd3
Qingtian <vfishv@gmail.com>
2019-10-10 15:06:04 +0800

    androidx

--- app/src/main/java/com/gjiazhe/layoutswitch/ItemAdapter.java
+++ app/src/main/java/com/gjiazhe/layoutswitch/ItemAdapter.java
@@ -1,7 +1,7 @@
 package com.gjiazhe.layoutswitch;
 
-import android.support.v7.widget.GridLayoutManager;
-import android.support.v7.widget.RecyclerView;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
--- app/src/main/java/com/gjiazhe/layoutswitch/MainActivity.java
+++ app/src/main/java/com/gjiazhe/layoutswitch/MainActivity.java
@@ -1,10 +1,10 @@
 package com.gjiazhe.layoutswitch;
 
-import android.support.v7.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
-import android.support.v7.app.AppCompatDelegate;
-import android.support.v7.widget.GridLayoutManager;
-import android.support.v7.widget.RecyclerView;
+import androidx.appcompat.app.AppCompatDelegate;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 import android.view.Menu;
 import android.view.MenuItem;
 
